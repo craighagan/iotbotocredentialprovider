9fa634ab491ad1482d5a97df6a045c41811bfba9
Craig Hagan <hagan@amazon.com>
2018-04-26 19:14:33 -0400

    add license
    allow different ports for fakemetadata server
    
    fix
    
    update version

--- bin/fakemetadata-server.py
+++ bin/fakemetadata-server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from iotbotocredentialprovider.FakeMetadata import FakeMetadataServer, FakeMetadataRequestHandler
+import argparse
+from iotbotocredentialprovider.FakeMetadata import FakeMetadataServer, FakeMetadataRequestHandler, PORT
 
 # this will require that
 # the following be set:
@@ -7,5 +8,10 @@ from iotbotocredentialprovider.FakeMetadata import FakeMetadataServer, FakeMetad
 # /sbin/iptables -t nat -A OUTPUT -p tcp -d 169.254.170.2   --dport 80 -j DNAT --to-destination 127.0.0.1:51680
 
 if __name__ == "__main__":
-    f = FakeMetadataServer(FakeMetadataRequestHandler)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, dest="port",
+                        help="port to listen on defaults to %s" % PORT, required=False, default=PORT)
+    args = parser.parse_args()
+
+    f = FakeMetadataServer(FakeMetadataRequestHandler, port=args.port)
     f.run()
